25236cc7b8531693826032fbc708e301f475b190
panchorc1 <panchorc1@gmail.com>
2020-04-08 23:04:23 -0600

    detectar en la vista si hay un cambio de entradas en el input

--- portafolio/src/app/components/user/user.component.ts
+++ portafolio/src/app/components/user/user.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  styleUrls: ['./user.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+
 })
 export class UserComponent implements OnInit {
 

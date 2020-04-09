54595fa7673a5eb1143ac2b78795aa32b70d1d3e
panchorc1 <panchorc1@gmail.com>
2020-04-08 21:27:57 -0600

    Creando nuestra clase model

--- portafolio/src/app/app.module.ts
+++ portafolio/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { GithubService } from './services/github.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [

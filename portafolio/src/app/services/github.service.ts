d4f3e396e10983dd7d2f848ec8ee7962e4600e62
panchorc1 <panchorc1@gmail.com>
2020-04-08 21:51:41 -0600

    creando el servicio

--- portafolio/src/app/services/github.service.ts
+++ portafolio/src/app/services/github.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getUser(name: string): Observable<User> {
+    const url = `https://api.github.com/users/${name}`;
+    return this.http.get<User>(url);
+  }
 }
